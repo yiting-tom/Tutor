1e4e5b5da7e0efa1bf1e2cb608740a09dca93bd2
Li-Yi-Ting <liyitingtom@gmail.com>
2020-03-06 10:53:03 +0800

    mod

--- C/course/01_Basic_Concept/main3.c
+++ C/course/01_Basic_Concept/main3.c
@@ -3,9 +3,15 @@
 
 int main(void) {
 
-  int i;
+  int i = 10;
   
-  printf("%d", i);
+  /*========= if-else 條件句 =========*/
+
+  if (i == 10) {              //如果i是10的話...
+    printf("i is 10\n");
+  } else {
+    printf("i isn't 10.\n");  //不然就...
+  }
 
 
   return 0;
